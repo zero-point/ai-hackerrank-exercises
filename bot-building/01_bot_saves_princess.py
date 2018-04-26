d4c0a66cf0bab0c3c71c6463a57049f58d3d22fd
zero-point <irinapreda91@gmail.com>
2018-04-26 23:06:22 +0100

    added first exe

---
+++ bot-building/01_bot_saves_princess.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def displayPathtoPrincess(n,grid):
+#print all the moves here
+m = int(input())
+grid = [] 
+for i in range(0, m): 
+    grid.append(input().strip())
+
+displayPathtoPrincess(m,grid)
